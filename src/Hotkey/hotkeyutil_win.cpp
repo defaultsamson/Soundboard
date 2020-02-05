b4829276050a02db8006ceb7f8ae494480a1db63
Samson <qwertysam@users.noreply.github.com>
2020-02-04 20:38:00 -0800

    Windows key naming works

--- src/Hotkey/hotkeyutil_win.cpp
+++ src/Hotkey/hotkeyutil_win.cpp
@@ -5,7 +5,14 @@
 #include <qt_windows.h>
 
 QString HotkeyUtil::keycodeToString(quint32 keycode) {
-    return QString::number(keycode);
+    // https://stackoverflow.com/questions/38100667/windows-virtual-key-codes
+    int size = 1024;
+    char* name = static_cast<char*>(malloc(size * sizeof(char)));
+    long lParamValue = keycode << 16;
+    GetKeyNameTextA(lParamValue, name, size);
+    QString ret = QString(name);
+    free(name);
+    return ret;
 }
 
 QHotkey::NativeShortcut HotkeyUtil::scancodeToNative(quint32 k) {
